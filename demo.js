6f225783b2e8b33af051b3b5c8849ee292705b3b
E-Molina21 <146690811+E-Molina21@users.noreply.github.com>
2025-10-02 19:46:48 -0600

    Update demo.js

--- demo.js
+++ demo.js
@@ -27,8 +27,13 @@ var mapa = new L.map('mapa', {
 var layerControl = L.control.layers(capasBase, null, {position: 'topleft'}).addTo(mapa);
 //Cargar Sidebar
 
-var sidebar = L.control.sidebar({sidebar}).addTo(mapa);
-
+//var sidebar = L.control.sidebar({sidebar}).addTo(mapa);
+var sidebar = L.control.sidebar({
+    autopan: false,       // whether to maintain the centered map point when opening the sidebar
+    closeButton: true,    // whether t add a close button to the panes
+    container: 'sidebar', // the DOM container or #ID of a predefined sidebar container that should be used
+    position: 'left',     // left or right
+}).addTo(mapa);
 
 
 var coordsBox = L.control({
@@ -49,6 +54,7 @@ coordsBox.addTo(mapa);
 mapa.on('mousemove', function (e) {
 	coordsBox.update(e.latlng);
 });
+
 
 
 
