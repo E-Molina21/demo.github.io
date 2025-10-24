8a607b50d69e016a4859622117e0ea12959a6f27
E-Molina21 <146690811+E-Molina21@users.noreply.github.com>
2025-10-24 09:42:38 -0600

    Update demo.js

--- demo.js
+++ demo.js
@@ -71,6 +71,7 @@ const staticlay = {
 
 const dynlay = {
 	"Ciclones Tropicales NOAA": {
+		type: "wms",
 		url:"http://servicios2.cenapred.unam.mx:6080/arcgis/rest/services/ServAuto/NoaaCT/MapServer",
 		layers:[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29],
 		opacity: 0.7,
@@ -176,6 +177,7 @@ Object.entries(dynlay).forEach(([name, info]) => {
   label.append(" " + name);
   layerListdyn.appendChild(label);
 });
+
 
 
 
