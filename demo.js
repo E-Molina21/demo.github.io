7d36cee8aaf5d12d2f1abb4e924287647c9b9ca0
E-Molina21 <146690811+E-Molina21@users.noreply.github.com>
2025-11-07 15:19:36 -0600

    Update demo.js

--- demo.js
+++ demo.js
@@ -74,7 +74,7 @@ const staticlay = {
   "Mapa Bivariado unidades económicas": {
     type: "image",
     url: "BIVARADOagricVSindust.png",
-	bounds: [[35.498,-120.058],[10.752,-85.060]],
+	bounds: [[35.528,-120.058],[11.052,-85.060]],
 	options: {opacity: 0.7},
     layer: null
   },
@@ -327,6 +327,7 @@ Object.entries(dynlay).forEach(([name, info]) => {
   label.append(" " + name);
   layerListdyn.appendChild(label);
 });
+
 
 
 
